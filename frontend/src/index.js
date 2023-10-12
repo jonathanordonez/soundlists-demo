6cca9f6446521934ecc5f5a8c56dd5b8613cfcac
Jonathan Ordonez <jonathan_oliver05@hotmail.com>
2023-10-12 00:04:04 -0400

    Updated index.js to disable logging in Production

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,9 +4,9 @@ import "./index.css";
 import App from "./Components/App";
 import { BrowserRouter as Router } from "react-router-dom";
 
-// if (process.env.REACT_APP_ENVIRONMENT === "production") {
-//   console.log = () => {};
-// }
+if (process.env.REACT_APP_ENVIRONMENT === "production") {
+  console.log = () => {};
+}
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
